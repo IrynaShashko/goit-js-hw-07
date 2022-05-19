20f5b21958b2293e9c944eee2b2ae81c3ac4401a
IrynaShashko <irishkashashko@gmail.com>
2022-05-19 15:32:20 +0300

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryItems(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+        />
+    </a>
+    </div>
+     `
+    }).join("");
+};
+
+
+imageContainer.addEventListener("click",onImageContainerClick);
+
+function onImageContainerClick(evt) {
+    evt.preventDefault();
+    evt.target.src = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.src = evt.target.dataset.source}" width="1000" height="800">
+`)
+instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imageContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryItems(galleryItems);
+imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+        />
+    </a>
+    </div>
+     `
+    }).join("");
+};
