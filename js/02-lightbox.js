6d9ad48899aa6856e22a70f01b4613149ece986e
IrynaShashko <irishkashashko@gmail.com>
2022-05-20 17:01:42 +0300

    add 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -11,13 +11,8 @@ function createGalleryItems(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
     <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-        />
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
     </div>
      `
